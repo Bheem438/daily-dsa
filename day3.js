a3f11a06f816e1f911abfcbcc5c7867aa581b0a0
vindokum <vinodkumar3693@gmail.com>
2026-02-04 22:16:01 +0530

    Day3: Solved Linear and Binary Search algorithms

---
+++ day3.js
@@ -0,0 +1,31 @@
+function linearSearch(arr, target){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] === target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+console.log(linearSearch([1,2,3,4,6,8,0], 0))
+
+// Binary Search
+
+function binarySearch(arr, target){
+    let left = 0;
+    let right = arr.length -1;
+
+    while(right >= left){
+        const mid = Math.floor((left+right)/2);
+
+        if(arr[mid] === target){
+            return mid;
+        }else if(arr[mid] > target){
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return -1;
+}
+console.log(binarySearch([1,2,3,4,6,8], 8))
